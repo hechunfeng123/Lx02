9dbbc14495b266ee99bb4147006b19c7cf6dd2d6
hechunfeng123 <54524340+hechunfeng123@users.noreply.github.com>
2019-09-04 12:14:55 +0800

    Update UnitTest1.cs

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -13,5 +13,19 @@ namespace UnitTestProject1
         {
             Console.WriteLine(DAL.UserInfo.Instance.UserCheck("Admin"));
         }
+         [TestMethod]
+        public void TestMethod2() {
+            Model.UserInfo user = new Model.UserInfo { userName = "Guest", passWord = "sql.123", type = "管理员" };
+            Console.WriteLine(DAL.UserInfo.Instance.Add(user));
+            Console.WriteLine(JsonConvert.SerializeObject(DAL.UserInfo.Instance.GetAll()));
+            user.qq = "104512708";
+            Console.WriteLine(DAL.UserInfo.Instance.Update(user));
+            var model = DAL.UserInfo.Instance.GetModel("Guest");
+            Console.WriteLine(JsonConvert.SerializeObject(model));
+            Console.WriteLine(DAL.UserInfo.Instance.Delete("Guest"));
+            var page = DAL.UserInfo.Instance.GetPage(new Model.page { pageIndex = 2, pageSize = 2 });
+            Console.WriteLine(JsonConvert.SerializeObject(page));
+            Console.WriteLine(DAL.UserInfo.Instance.GetCount());
+        }
     }
 }
